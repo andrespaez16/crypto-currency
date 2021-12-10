567f289f278dc13a30384dea4e23cea34396d038
andres fernando paez <andres.paez@avista.co>
2021-12-09 20:57:52 -0500

    ready scafold

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import { Exchanges, HomePage, CryptoDetails, Navbar } from "./components";
+import "./App.css";
+
+const App = () => (
+  <BrowserRouter>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+            </Routes>
+          </div>
+        </Layout>
+      </div>
     </div>
-  );
-}
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as HomePage } from './HomePage';
+export { default as Navbar } from './Navbar';
+export { default as News } from './News';
+export { default as Footer } from './Footer';
+export { default as Exchanges } from './Exchanges';
+export { default as CryptoDetails } from './CryptoDetails';
